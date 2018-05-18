6cf6e6b404902ae1706e7395fddfbe24e80681d2
Matheus Raz <mrol@cin.ufpe.br>
2018-05-18 13:57:05 -0300

    correção de porta hardcoded

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const app = express()
-const port = 80
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-app.listen(process.env.PORT || port, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
     return console.log('algo errado aconteceu', err)
   }
 
-  console.log(`server escutando na porta ${port}`)
+  console.log(`server escutando na porta 3000`)
 })
