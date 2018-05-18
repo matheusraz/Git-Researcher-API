aa41ce761f2aeeaf446a5f7e2c0426ba648be573
Matheus Raz <mrol@cin.ufpe.br>
2018-05-18 15:38:44 -0300

    adição de postinstall para gerar build da app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const app = express()
 
-let port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
