442633226ee611a36dca9cc33f5cc4bacdff0dbc
Matheus Raz <mrol@cin.ufpe.br>
2018-05-18 14:35:31 -0300

    set variavel de ambiente no heroku

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const app = express()
 
+let port = process.env.PORT || 80;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-app.listen(process.env.PORT || 3000, (err) => {
+app.listen(port, (err) => {
   if (err) {
     return console.log('algo errado aconteceu', err)
   }
 
-  console.log(`server escutando na porta 3000`)
+  console.log(`server escutando na porta ${port}`);
 })
