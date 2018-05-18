fa3304574eb02003c1a78dbabbd5f8ad80fbf4d8
Matheus Raz <mrol@cin.ufpe.br>
2018-05-18 13:28:57 -0300

    set para porta 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const app = express()
-const port = 3000
+const port = 80
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
